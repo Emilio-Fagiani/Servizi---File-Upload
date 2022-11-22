789e5a7d310aa42ccbc5a28b82d234bcc9debf07
Emilio <emi00@gmail.com>
2022-11-22 15:24:24 +0100

    First commit

---
+++ src/test/java/it/develhope/Servizi/File/Upload/ServiziFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.Servizi.File.Upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiziFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
